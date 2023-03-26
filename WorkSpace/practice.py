cc6f41dff67e9d741ba060e892961871b6e75bc8
pchh04 <128962405+pchh04@users.noreply.github.com>
2023-03-27 03:11:35 +0900

    Delete WorkSpace directory

--- WorkSpace/practice.py
+++
@@ -1,5 +0,0 @@
-from math import log10
-a,b = map(int,input('? ').split())
-while all((a,b)):
-    print(sum([(lambda x : (a%x+b%x)/x)(10**(n+1)) for n in range(int(max(log10(a),log10(b)))+1)]))
-    a,b = map(int,input('? ').split())
